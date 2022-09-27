f8b6dad4ef5e9e22dd1605544f03ca01ca523e4b
Toni <107318721+Toni2Morales@users.noreply.github.com>
2022-09-27 14:07:20 +0200

    commit3

--- pagina.py
+++ pagina.py
@@ -1,5 +1,6 @@
 from flask import Flask, request
 import sqlite3
+import pickle
 
 app = Flask(__name__)
 app.config["Debug"] = True
@@ -7,6 +8,15 @@ app.config["Debug"] = True
 @app.route("/", methods = ["GET"])
 def entrada():
     return("¡Bienvenido a la página web de Antonio!")
-    
+
+@app.route("/prediccion", methods=['GET'])
+def prediction():
+    tv = request.args.get("tv", None)
+    radio = request.args.get("radio", None)
+    newspaper = request.args.get("newspaper", None)
+    with open("data/advertising_model", "rb") as f:
+        modelo = pickle.load(f)
+    prediccion = modelo.predict([[tv, radio, newspaper]])
+    return str(prediccion)
 
 
